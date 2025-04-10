6f8f3327e1db467144e339849a1d4b4e78a27ea0
Arjun Bindhu Suresh <arjunbsuresh@gmail.com>
2025-04-10 17:04:56 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Arjun ECS Container." 
+def home():
+    return "Hello from Arjun ECS Container."
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
