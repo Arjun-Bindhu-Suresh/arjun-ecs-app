aae26a9e12abe00464a2e7b049509315e867d602
Arjun Bindhu Suresh <arjunbsuresh@gmail.com>
2025-04-11 05:13:19 -0400

    Trigger pipeline test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Arjun ECS Container."
+    return "Hello from Arjun CI/CD Pipeline! 🚀"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
